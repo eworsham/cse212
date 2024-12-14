8c1c2df8f592996efdee3e3af3dd6861fe586526
eworsham <eworsham64@gmail.com>
2024-12-13 19:42:38 -0500

    W06 Code: Problems 1, 2 and 3

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -21,7 +21,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -34,6 +34,18 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data)
+        {
+            return true;
+        }
+        else if ((value < Data) && (Left != null))
+        {
+            return Left.Contains(value);
+        }
+        else if ((value > Data) && (Right != null))
+        {
+            return Right.Contains(value);
+        }
         return false;
     }
 
